b7e011aa3334ba6bc58f3f030139e58a38ca89db
mdrasel97 <mollarasel972@gmail.com>
2026-01-12 18:33:07 +0600

    PrismaClientValidationError added

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,12 +1,29 @@
 import { NextFunction, Request, Response } from "express"
+import { Prisma } from "../../generated/prisma/client"
+
 
 function errorHandler (err:any, req:Request, res:Response, next: NextFunction) {
-//   if (res.headersSent) {
-//     return next(err)
-//   }
-  res.status(500)
+
+  let statusCode = 500
+  let errorMessage = "internal server error"
+  let errorDetails = err
+
+
+  // prisma client validation error 
+  if(err instanceof Prisma.PrismaClientValidationError){
+    statusCode = 400
+    errorMessage = "You provide incorrect field type"
+  }else if(err instanceof Prisma.PrismaClientKnownRequestError ){
+    if(err.code === "P2025"){
+      statusCode = 400
+      errorMessage= "An operation failed because it depends on one more records"
+    }
+  }
+
+  res.status(statusCode)
   res.json({
-    message: "Error From Error Handler"
+    message: errorMessage,
+    error : errorDetails
   })
 }
 
